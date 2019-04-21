83c41558d9f13fab705845d9de49c2fa58c62d3f
Felix Klauke <info@felix-klauke.de>
2019-04-21 07:41:09 +0200

    Extend build script to own file.
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

---
+++ build/buildPackage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+################
+### Clean up ###
+################
+rm -rf dist
+
+################################
+### Compile packaged version ###
+################################
+lessc src/style.less dist/style.css
+
+######################
+### Copy mix files ###
+######################
+mkdir dist/components
+cp -R src/components/* dist/components/
